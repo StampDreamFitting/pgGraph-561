dda0a65d81d4eb200fc16ffc2c3ab71f3f2f38ba
StampDreamFitting <arya5106banks@gmail.com>
2026-06-06 16:32:37 +0200

    Add file

---
+++ docs/_meta.js
@@ -0,0 +1,13 @@
+export default {
+  index: 'Overview',
+  quickstart: 'Quickstart',
+  'release-notes': 'Release Notes',
+  roadmap: 'Roadmap',
+  'known-issues': 'Known Issues',
+  user_guide: {
+    title: 'User Guide'
+  },
+  contributor_guide: {
+    title: 'Contributor Guide'
+  }
+}
